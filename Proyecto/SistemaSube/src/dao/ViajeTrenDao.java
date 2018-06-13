17b9a3750d568d61a0d259b1e561d1567410d7ba
MauroLucas <m_l_pereyra@hotmail.com>
2018-06-12 22:59:26 -0300

    Modifico clases dao

--- Proyecto/SistemaSube/src/dao/ViajeTrenDao.java
+++ Proyecto/SistemaSube/src/dao/ViajeTrenDao.java
@@ -1,6 +1,11 @@
 package dao;
 
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import datos.Viaje;
 import datos.ViajeTren;
+import funciones.Funciones;
 
 public class ViajeTrenDao extends DAO{
 	
@@ -12,6 +17,20 @@ public class ViajeTrenDao extends DAO{
 		if (instancia == null)
 			instancia = new ViajeTrenDao();
 		return instancia;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Viaje> traerViajesSubte(GregorianCalendar ini,GregorianCalendar fin,long idTransporte){
+		List<Viaje> viajes;
+		try {
+			iniciaOperacion();
+			String sql= "from ViajeTren v where v.fechaHora>='" + Funciones.traerFechaCortaHora(ini) + "' and v.fechaHora<='" + Funciones.traerFechaCortaHora(fin)+"'" + 
+			" and v.transporte.idTransporte="+idTransporte+ " order by v.fechaHora ASC";
+			viajes = session.createQuery(sql).list();
+		}finally {
+			session.close();
+		}
+		return viajes;
 	}
 	
 
