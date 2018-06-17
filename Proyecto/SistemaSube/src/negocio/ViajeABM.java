c771dc972bc3fca2800019a9f44c6eeacf39f46a
MauroLucas <m_l_pereyra@hotmail.com>
2018-06-17 19:47:52 -0300

    Adjunto funcionalidad de estadisticas a ViajeABM

--- Proyecto/SistemaSube/src/negocio/ViajeABM.java
+++ Proyecto/SistemaSube/src/negocio/ViajeABM.java
@@ -1,11 +1,19 @@
 package negocio;
 
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.List;
 
 import dao.ViajeDao;
+import dao.ViajeSubteDao;
+import dao.ViajeColectivoDao;
+import dao.ViajeTrenDao;
 import datos.Viaje;
+import datos.ViajeColectivo;
+import datos.ViajeSubte;
+import datos.ViajeTren;
 import funciones.Funciones;
+
 
 public class ViajeABM {
 	private static ViajeABM instance = null;
@@ -40,5 +48,68 @@ public class ViajeABM {
 	public List<Viaje> traerViajesSubte(GregorianCalendar ini,GregorianCalendar fin){
 		return ViajeDao.getInstance().traerViajesSubte(ini, fin);
 	}
+	
+	public List<ViajeSubte> traerViajesSubte(GregorianCalendar ini,GregorianCalendar fin,long idTransporte){
+		return ViajeSubteDao.getInstance().traerViajesSubte(ini, fin, idTransporte);
+	}
+	
+	public List<ViajeColectivo> traerViajesColectivo(GregorianCalendar ini,GregorianCalendar fin,long idTransporte){
+		return ViajeColectivoDao.getInstance().traerViajesColectivo(ini, fin, idTransporte);
+	}
+	
+	public List<ViajeTren> traerViajesTren(GregorianCalendar ini,GregorianCalendar fin,long idTransporte){
+		return ViajeTrenDao.getInstance().traerViajesTren(ini, fin, idTransporte);
+	}
+	
+	public Estadistica estadisticaCantViajesColectivo(GregorianCalendar ini,GregorianCalendar fin,long idTransporte){
+		Estadistica estadistica = new Estadistica();
+		List<ViajeColectivo> viajesColectivos = ViajeColectivoDao.getInstance().traerViajesColectivo(ini, fin, idTransporte);
+		for(ViajeColectivo viaje : viajesColectivos) {
+			Muestra muestra = estadistica.traerMuestra((int) viaje.getTramo().getIdTramo());
+			if(muestra==null) {
+				estadistica.add(new Muestra((int)viaje.getTramo().getIdTramo(),viaje.getTramo().getDistancia(),1));
+			}else {
+				muestra.setMuestra(muestra.getMuestra()+1);
+			}
+		}
+		
+		return estadistica;
+		
+	}
+	
+	public Estadistica estadisticaCantViajesSubte(GregorianCalendar ini,GregorianCalendar fin,long idTransporte){
+		Estadistica estadistica = new Estadistica();
+		List<ViajeSubte> viajeSubte = ViajeSubteDao.getInstance().traerViajesSubte(ini, fin, idTransporte);
+		for(ViajeSubte viaje : viajeSubte) {
+			Muestra muestra = estadistica.traerMuestra((int) viaje.getOrigen().getIdParada());
+			if(muestra==null) {
+				estadistica.add(new Muestra((int)viaje.getOrigen().getIdParada(),viaje.getOrigen().getNombre(),1));
+			}else {
+				muestra.setMuestra(muestra.getMuestra()+1);
+			}
+		}
+		
+		return estadistica;
+		
+	}
+	
+	public Estadistica estadisticaCantViajesTren(GregorianCalendar ini,GregorianCalendar fin,long idTransporte){
+		Estadistica estadistica = new Estadistica();
+		List<ViajeTren> viajeTren = ViajeTrenDao.getInstance().traerViajesTren(ini, fin, idTransporte);
+		for(ViajeTren viaje : viajeTren) {
+			Muestra muestra = estadistica.traerMuestra((int) viaje.getOrigen().getIdParada(),(int) viaje.getDestino().getIdParada());
+			if(muestra==null) {
+				estadistica.add(new Muestra((int)viaje.getOrigen().getIdParada(),(int) viaje.getDestino().getIdParada(),viaje.getOrigen().getNombre(),viaje.getDestino().getNombre(),1));
+			}else {
+				muestra.setMuestra(muestra.getMuestra()+1);
+			}
+		}
+		
+		return estadistica;
+		
+	}
+	
+	
+	
 
 }
