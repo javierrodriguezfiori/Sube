665e5d72b9149408029d880a24bfe3a29c78d307
MauroLucas <m_l_pereyra@hotmail.com>
2018-07-01 21:26:45 -0300

    fix controlador

--- Proyecto/SistemaSube/src/controladores/ControladorMostrarReportes.java
+++ Proyecto/SistemaSube/src/controladores/ControladorMostrarReportes.java
@@ -8,8 +8,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import funciones.Funciones;
 import java.util.GregorianCalendar;
+
+import negocio.TransportePublicoABM;
 import negocio.ViajeABM;
+import datos.Colectivo;
+import datos.Subte;
+import datos.TransportePublico;
+import datos.Tren;
 import datos.Viaje;
+import datos.ViajeColectivo;
+import datos.ViajeSubte;
+import datos.ViajeTren;
 
 public class ControladorMostrarReportes extends HttpServlet {
 	
@@ -25,55 +34,64 @@ public class ControladorMostrarReportes extends HttpServlet {
 		response.setContentType("text/html;charset=UTF-8");
 		try {
 			
-			String fechaInicio = request.getParameter("fechaInicio");
-			String horaInicio = request.getParameter("horaInicio");
-			String fechaFin = request.getParameter("fechaFin");
-			String horaFin = request.getParameter("horaFin");
-			int opcion = Integer.parseInt(request.getParameter("Transporte"));
-			GregorianCalendar fechaHoraInicio = Funciones.traerFecha(fechaInicio, horaInicio);
-			GregorianCalendar fechaHoraFin = Funciones.traerFecha(fechaFin,horaFin);
+			String fechaInicio = (String) request.getParameter("fechaInicio");
+			String fechaFin = (String) request.getParameter("fechaFin");
 			
-			if(opcion==1) {
+			
+			
+			GregorianCalendar fechaHoraInicio = parsearFechaEnTexto(fechaInicio);
+			GregorianCalendar fechaHoraFin = parsearFechaEnTexto(fechaFin);
+			
+			long idTransporte = Long.parseLong((String) request.getParameter("idLineaDeTransporte"));
+			
+			if(idTransporte==0) { //Reportes Generales por transporte//
+				String transporteEnTexto = (String) request.getParameter("transportePublico");
+				if(transporteEnTexto.equals("tren")) {
+					List<Viaje> viajesTren = ViajeABM.getInstance().traerViajesTren(fechaHoraInicio, fechaHoraFin);
+					request.setAttribute("viajesTren", viajesTren);
+                	request.getRequestDispatcher("ajaxreportetren.jsp").forward(request, response);
+				}
 				
+                if(transporteEnTexto.equals("colectivo")) {
+                	List<Viaje> viajesColectivo = ViajeABM.getInstance().traerViajesColectivo(fechaHoraInicio, fechaHoraFin);
+                	request.setAttribute("viajesColectivo", viajesColectivo);
+                	request.getRequestDispatcher("ajaxreportecolectivo.jsp").forward(request, response);
+				}
+                if(transporteEnTexto.equals("subte")) {
+                	List<Viaje> viajesSubte = ViajeABM.getInstance().traerViajesSubte(fechaHoraInicio, fechaHoraFin);
+                	request.setAttribute("viajesSubte", viajesSubte);
+                	request.getRequestDispatcher("ajaxreportesubte.jsp").forward(request, response);
+					
+				}
+			}
+			else {   // Reportes por linea de Transporte//
 				
-				
-				List<Viaje> viajesTren = ViajeABM.getInstance().traerViajesTren(fechaHoraInicio, fechaHoraFin);
-				List<Viaje> viajesColectivo = ViajeABM.getInstance().traerViajesColectivo(fechaHoraInicio, fechaHoraFin);
-				List<Viaje> viajesSubte = ViajeABM.getInstance().traerViajesSubte(fechaHoraInicio, fechaHoraFin);
-				
+			
+			
+			TransportePublico transportePublico = TransportePublicoABM.getInstance().traer(idTransporte);
+			
+			if(transportePublico instanceof Tren) {
+				List<ViajeTren> viajesTren = ViajeABM.getInstance().traerViajesTren(fechaHoraInicio, fechaHoraFin, transportePublico.getIdTransporte());
 				request.setAttribute("viajesTren", viajesTren);
+				request.setAttribute("tren", (Tren)transportePublico);
+				request.getRequestDispatcher("ajaxreportetrenporlinea.jsp").forward(request, response);
+			}
+			
+			if(transportePublico instanceof Colectivo) {
+				List<ViajeColectivo> viajesColectivo = ViajeABM.getInstance().traerViajesColectivo(fechaHoraInicio, fechaHoraFin,transportePublico.getIdTransporte());
 				request.setAttribute("viajesColectivo", viajesColectivo);
+				request.setAttribute("colectivo", (Colectivo)transportePublico);
+				request.getRequestDispatcher("ajaxreportecolectivoporlinea.jsp").forward(request, response);
+				
+			}
+			
+			if(transportePublico instanceof Subte) {
+				List<ViajeSubte> viajesSubte = ViajeABM.getInstance().traerViajesSubte(fechaHoraInicio, fechaHoraFin,transportePublico.getIdTransporte());
 				request.setAttribute("viajesSubte", viajesSubte);
-				
-				request.getRequestDispatcher("ajaxreportes.jsp").forward(request, response);
-				
-				
+				request.setAttribute("subte", (Subte)transportePublico);
+				request.getRequestDispatcher("ajaxreportesubteporlinea.jsp").forward(request, response);
 			}
-			
-			if(opcion==2){
-				List<Viaje> viajesTren = ViajeABM.getInstance().traerViajesTren(fechaHoraInicio, fechaHoraFin);
-				request.setAttribute("viajesTren", viajesTren);
-				request.getRequestDispatcher("ajaxreportetren.jsp").forward(request, response);
-				
-			}
-			
-
-			if(opcion==3){
-				List<Viaje> viajesColectivo = ViajeABM.getInstance().traerViajesColectivo(fechaHoraInicio, fechaHoraFin);
-				request.setAttribute("viajesColectivo", viajesColectivo);
-				request.getRequestDispatcher("ajaxreportecolectivo.jsp").forward(request, response);
-				
-			}
-
-			if(opcion==4){
-				List<Viaje> viajesSubte = ViajeABM.getInstance().traerViajesSubte(fechaHoraInicio, fechaHoraFin);
-				request.setAttribute("viajesSubte", viajesSubte);
-				request.getRequestDispatcher("ajaxreportesubte.jsp").forward(request, response);
-				
-			}
-			
-			
-			
+		}
 			
 			
 
@@ -82,5 +100,22 @@ public class ControladorMostrarReportes extends HttpServlet {
 			response.sendError(500,"Fecha Incorrecta");
 		}
 		}
+	
+	private GregorianCalendar parsearFechaEnTexto(String fechaEnTexto) {
+		GregorianCalendar fechaFormateada = null;
+		
+		if (fechaEnTexto.isEmpty())
+			fechaFormateada = new GregorianCalendar();
+		else {
+			int dia = Integer.parseInt(fechaEnTexto.substring(0, 2));
+			int mes = Integer.parseInt(fechaEnTexto.substring(3, 5));
+			int anio = Integer.parseInt(fechaEnTexto.substring(6, 10));
+			int hora = Integer.parseInt(fechaEnTexto.substring(11, 13));
+			int minutos = Integer.parseInt(fechaEnTexto.substring(14, 16));
+			fechaFormateada = new GregorianCalendar(anio, mes - 1, dia, hora, minutos);
+		}
+		
+		return fechaFormateada;	
+	}
 
 }
