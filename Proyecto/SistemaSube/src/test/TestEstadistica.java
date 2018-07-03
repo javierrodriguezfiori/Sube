87c9ac725c784b944be6005b2bf62c66c43f9052
MauroLucas <m_l_pereyra@hotmail.com>
2018-07-03 16:11:59 -0300

    caso de prueba

--- Proyecto/SistemaSube/src/test/TestEstadistica.java
+++ Proyecto/SistemaSube/src/test/TestEstadistica.java
@@ -13,12 +13,12 @@ public class TestEstadistica {
 		//System.out.println(Funciones.imprimirLista(ViajeABM.getInstance().estadisticaViajeColectivo(ini, fin, (long)3).getMuestras()));
        // System.out.println(funciones.Funciones.imprimirLista(ViajeABM.getInstance().traerViajesColectivo(ini, fin,(long)3 )));
 	    //System.out.println(funciones.Funciones.imprimirLista(ViajeABM.getInstance().estadisticaViajeSubte(ini, fin, (long)2).getMuestras()));
-	   // System.out.println(funciones.Funciones.imprimirLista(ViajeABM.getInstance().estadisticaViajeTren(ini, fin, (long)1).getMuestras()));
+	   System.out.println(funciones.Funciones.imprimirLista(ViajeABM.getInstance().estadisticaViajesColectivo(ini, fin, (long)3).getMuestras()));
 	    
 	    
 	    //System.out.println(funciones.Funciones.imprimirLista(ViajeABM.getInstance().estadisticaSumMontosViajesTren(ini, fin, (long)1).getMuestras()));
 	   // System.out.println(funciones.Funciones.imprimirLista(ViajeABM.getInstance().estadisticaCantViajesTren(ini, fin, (long)1).getMuestras()));
-	    System.out.println(Funciones.imprimirLista(ViajeABM.getInstance().traerViajesTren(ini, fin)));
+	   // System.out.println(Funciones.imprimirLista(ViajeABM.getInstance().traerViajesTren(ini, fin)));
 	    //System.out.println(funciones.Funciones.imprimirLista(ViajeABM.getInstance().estadisticaSumMontosViajesColectivo(ini, fin, (long)3).getMuestras()));
 	}
 
